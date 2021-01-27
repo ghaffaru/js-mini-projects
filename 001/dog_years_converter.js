8ea32823699609486540271fdc75ef88bc3c9638
Ghaffaru <mudashiruagm@gmail.com>
2021-01-27 11:14:31 +0000

    dog years converter

---
+++ 001/dog_years_converter.js
@@ -0,0 +1,15 @@
+let myAge = 34;
+
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+
+laterYears *= 4;
+
+let ageInDogYears = earlyYears + laterYears;
+
+let myName = 'Ghaffaru'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${ageInDogYears} years old in dog years`);
